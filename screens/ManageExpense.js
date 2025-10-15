6a6b25fa5e06058705bb3ce3530ada3032f19655
04Marsha <161226959+04Marsha@users.noreply.github.com>
2025-10-15 16:56:53 +0530

    displaying items into different screens

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,7 @@
 import { Text } from "react-native";
 
 function ManageExpense() {
-    return <Text> Manage Expenses</Text>
+  return <Text> Manage Expenses </Text>;
 }
 
 export default ManageExpense;
